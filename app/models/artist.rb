0bce2965a7b948d18ab3940f49675f9822196a69
Silas Michael Knight <silasfelinus@gmail.com>
2021-03-15 07:59:50 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,7 @@
 class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
+  attr_accessor :songs, :genres
 
   def get_genre_of_first_song
     self.songs.first.genre
